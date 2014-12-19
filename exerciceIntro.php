bbbbbe304d9e42222c532379f1adeccdd30fd585
navec <navecbatchi@gmail.com>
2014-12-19 23:55:24 +0100

    exercice d'introduction à php

---
+++ exerciceIntro.php
@@ -0,0 +1,31 @@
+
+
+
+	<?php
+
+
+		// affichez "hello world" avec un echo
+
+		//créer une variable nommée "salutation", assignez lui une valeur de "bonjour", et affichez la valeur de cette variable
+
+		//additionnez 22390 + 9995193 et stockez le résultat dans une variable nommée "resultat"
+
+		//soustrayez 4992 à "resultat", et affichez la valeur de variable
+
+		//affichez les seulement les deux premiers chiffres de ce résultat
+
+		//créer un array nommé "prenoms", et ajoutez y des prénoms de personnes.
+
+		//affichez tous ces prénoms avec une boucle for
+
+		//idem, avec une boucle foreach
+
+		//idem, avec une boucle while.
+
+
+		//recréez le même array, mais ajoutez, pour chaque étudiant, un âge et un sexe
+
+		//affichez avec une boucle for le nom et le sexe de chaque étudiant
+
+
+		//même exercice, mais cette fois, le nom doit être affiché dans un h1, le sexe dans un span et l'âge dans un p

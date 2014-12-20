21ba93ee5ba20bd3a8698b97292d84f3f39c78c6
navec <navecbatchi@gmail.com>
2014-12-20 12:42:50 +0100

    foreach

--- exerciceIntro.php
+++ exerciceIntro.php
@@ -4,62 +4,68 @@
 	<?php
 		// affichez "hello world" avec un echo
 		echo "<p>hello world</p>";
-	?>
 	
-	<?php
+
+	
+	
 		//créer une variable nommée "salutation", assignez lui une valeur de "bonjour", et affichez la valeur de cette variable
 		$salutation = "bonjour";
-		echo "$salutation";
-	?>
-	</br></br>
+		echo $salutation."</br></br>";
 	
-	<?php
+
+	
+	
+	
 		//additionnez 22390 + 9995193 et stockez le résultat dans une variable nommée "resultat"
 		$addition = 22390 + 9995193;
 		$resultat = $addition;
-		echo "$addition";
+		echo $addition."</br></br>";
 		
 		
-	?>
-	</br></br>
 	
-	<?php
+
+	
+	
+	
 			//soustrayez 4992 à "resultat", et affichez la valeur de variable
 		$chiffre3 = 4992;
 		echo $resultat - $chiffre3 . ' </br> ';
-		echo substr($resultat, 0, 2) ;
+		echo substr($resultat, 0, 2). ' </br> ' ;
 		
-	?>
+	
+
 		
 		
 		//affichez les seulement les deux premiers chiffres de ce résultat
 	
 		
 		
-	<?php
+	
 		//créer un array nommé "prenoms", et ajoutez y des prénoms de personnes.
 		$prenoms = Array('Guillaume', 'Gaël', 'Julien');
 		
 		//affichez tous ces prénoms avec une boucle for
 		for($numero = 0; $numero < 3; $numero++)
 		{
-		echo '<p>' . $prenoms[$numero] . '</p>';
+		echo '<p>' . $prenoms[$numero] . '</p><br/>';
 		}
-	?>
+	
+
 		
 		
-	<?php
+	
 		$prenoms = Array('Guillaume', 'Gaël', 'Julien');
 		//idem, avec une boucle foreach
 		
-		foreach($prenoms as $prenom)
+		foreach($prenoms as $key => $prenom)
 		{
-		echo '<p>' . $prenom . '</p>';
+		echo '<p>'.$key.' = '. $prenom . '</p>';
 		}
-	?>
+	
+
 		
 
-		<?php// ! PAS REUSSI !
+		// ! PAS REUSSI !
 		//$prenoms = Array('Guillaume', 'Gaël', 'Julien');
 		//idem, avec une boucle while.
 		//$ligne = 0;
@@ -69,9 +75,10 @@
 		//echo '<p>' . $prenoms[$ligne] . '</p>';
 		//}
 		//$ligne++;
-	?>
+	
 
-	<?php
+
+	
 		$prenoms = Array('prenom' => 'Guillaume', 'age' => '19', 'sexe' => 'homme',
 						 'prenom' => 'Gaël', 'age' => '42', 'sexe' => 'hommme',
 						 'prenom' => 'Julien', 'age' =>'8', 'sexe' => 'femme');
@@ -82,11 +89,14 @@
 		{
 		echo '<p>' . $prenoms[$personne] . '</p>';
 		}
-		?>
+		
+
 		
 		
-		! PAS REUSSI !
+		// ! PAS REUSSI !
 		//affichez avec une boucle for le nom et le sexe de chaque étudiant
 
-		! PAS REUSSI !
+		// ! PAS REUSSI !
 		//même exercice, mais cette fois, le nom doit être affiché dans un h1, le sexe dans un span et l'âge dans un p  
+
+	?>
